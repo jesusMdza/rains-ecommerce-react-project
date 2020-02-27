4476c881a02f6e302816e341003fc5e04e5264b5
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-02-27 15:49:31 -0600

    adjust jsx structure

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -10,8 +10,23 @@ const Slider = ({next, previous, changeToIndexZero, changeToIndexTwo}) => {
 
   return (
     <div className="slider" onClick={(e) => handleClick(e)}>
-      <div className="left-arrow" onClick={() => previous()} onMouseEnter={changeToIndexZero} onMouseLeave={changeToIndexTwo}><img src="/images/arrows/arrow-left.png" alt="" /></div>
-      <div className="right-arrow" onClick={() => next()} onMouseEnter={changeToIndexZero} onMouseLeave={changeToIndexTwo}><img src="/images/arrows/arrow-right.png" alt="" /></div>
+      <div 
+        className="left-arrow" 
+        onClick={() => previous()} 
+        onMouseEnter={changeToIndexZero} 
+        onMouseLeave={changeToIndexTwo}
+      >
+        <img src="/images/arrows/arrow-left.png" alt="" />
+      </div>
+
+      <div 
+        className="right-arrow" 
+        onClick={() => next()} 
+        onMouseEnter={changeToIndexZero} 
+        onMouseLeave={changeToIndexTwo}
+      >
+        <img src="/images/arrows/arrow-right.png" alt="" />
+      </div>
     </div>
   );
 }
